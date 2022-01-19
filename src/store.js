e5d3fdb494897d2f69872385b445eaaccd0f005e
IreneBioque <irenebioque@gmail.com>
2022-01-19 14:49:36 +0100

    add ls

--- src/store.js
+++ src/store.js
@@ -1,11 +1,26 @@
 // Solo se puede tener un store
 
-import { createStore } from 'redux'
-import reducer from "./reducers"
+import { createStore } from 'redux';
+import reducer from "./reducers";
+import {getStateLocalStorage, setStateLocalStorage} from './utils/localStorage'
+
+ const localStorageState = getStateLocalStorage()
 
 const store = createStore(
     reducer, /* preloadedState, */
+
+    // así recuperamos los datos del ls
+    localStorageState,
+    
     // Con esto se muestra en la consola los errores
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  // para estar atentos a cualquier cambio de nuestro storage
+
+  store.subscribe(() => {
+    setStateLocalStorage({
+      tweets: store.getState().tweets
+    })
+  })
 export default store;
