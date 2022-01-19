df12069a9bb2c427761c42fc91b3309c5660230a
IreneBioque <irenebioque@gmail.com>
2022-01-19 12:36:15 +0100

    fixed error store

--- src/store.js
+++ src/store.js
@@ -6,6 +6,6 @@ import reducer from "./reducers"
 const store = createStore(
     reducer, /* preloadedState, */
     // Con esto se muestra en la consola los errores
- +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 export default store;
